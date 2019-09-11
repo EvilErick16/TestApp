4a4a3f3db9c6471e205c76cb3e8b7466c3bfba52
EvilErick16 <juarez.erick16@csu.fullerton.edu>
2019-09-10 18:04:08 -0700

    Add test code

--- app/src/main/java/com/csuf/cpsc411/TestApp/MainActivity.java
+++ app/src/main/java/com/csuf/cpsc411/TestApp/MainActivity.java
@@ -3,12 +3,39 @@ package com.csuf.cpsc411.TestApp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+//import androidx.viewpager.widget.ViewPager;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.view.ViewGroup;
+import android.graphics.Typeface;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        // 1. Create a TextView object
+        TextView v = new TextView(this);
+
+        // 2. Set view  attributes
+        v.setText("This is a test string");
+        v.setBackgroundColor(Color.YELLOW);
+        v.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM);
+        v.setPadding(50, 50, 50, 350);
+        v.setTextSize(36);
+        v.setTypeface(null, Typeface.BOLD);
+
+        // 3. Create Layout object
+        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+        v.setLayoutParams(lp);
+
+        // Set the content view
+        //setContentView(v);
+        setContentView(R.layout.single_widget_layout);
     }
 }
